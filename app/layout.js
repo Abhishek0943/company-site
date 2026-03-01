8dc735084a6ab8af0e115d04f80231aa0c9ace06
abhishek0943 <abhishekpundir077@gmail.com>
2026-03-01 16:51:58 +0530

    feat: add Google Analytics tracking ID

--- app/layout.js
+++ app/layout.js
@@ -86,6 +86,8 @@ export const metadata = {
   },
 };
 
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${syne.variable} ${dmSans.variable} ${jetbrains.variable}`}>
@@ -93,6 +95,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main>{children}</main>
         <Footer />
+        <GoogleAnalytics gaId="G-22YFGLHGN1" />
       </body>
     </html>
   );
